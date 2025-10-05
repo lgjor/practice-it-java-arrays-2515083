25d56e94fecccfab45641ce8c17fb930c94af889
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-10-05 20:23:35 -0300

    working with array data

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 public class App {
-  
+
   public static void main(String[] args) {
-    
+    double[] lotteryNums = {10, 15, 48, 54, 77}; // new double[5];
+    System.out.println(lotteryNums[2]);
+    lotteryNums[2] = 49;
+    System.out.println(lotteryNums[4]);
   }
 }
